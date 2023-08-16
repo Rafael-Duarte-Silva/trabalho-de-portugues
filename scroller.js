02a834921412d9a9a25b98d025adee49cdb831e7
Isac Silva De Oliveira <104024902+Isac471@users.noreply.github.com>
2023-08-16 08:51:04 -0300

    Delete scroller.js

--- scroller.js
+++
@@ -1,8 +0,0 @@
-const navMenu = document.querySelector("nav");
-
-document.addEventListener("scroll", () => {
-    if(window.scrollY > 50) {
-        return navMenu.classList.add("scrolled");
-    }
-    navMenu.classList.remove("scrolled");
-})
